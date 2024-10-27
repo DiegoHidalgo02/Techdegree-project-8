f981000d6ca0278a7da3496f72568d34bfea6ca3
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-10-27 22:09:04 +0100

    Refactor overlay styles, add open functionality

--- js/overlay.js
+++ js/overlay.js
@@ -1,4 +1,16 @@
 const overlay = document.querySelector(".overlay");
+const employeesContainer = document.querySelector(".employeesContainer");
+
+employeesContainer.addEventListener("click", event => {
+
+    const card = event.target.closest(".employee");
+
+    if(!card) return;
+
+    overlay.classList.remove("close");
+    overlay.classList.add("open");
+
+});
 
 overlay.addEventListener("click", event => {
 
