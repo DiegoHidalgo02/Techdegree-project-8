190957e8511cf41314259d9a3667ce793a50be2b
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-11-04 23:05:11 +0100

    Create functions to manage the Modal Slider, to move between cards and update the Modal Card. Add IDs for identifying Modal Card paragraphs. Remove import of employees array unused

--- js/overlay.js
+++ js/overlay.js
@@ -1,7 +1,70 @@
-import { employees } from "./filter.js";
+import { employeesFiltered } from "./filter.js";
+let index = 0;
+let employee;
 
 const overlay = document.querySelector(".overlay");
 const employeesContainer = document.querySelector(".employeesContainer");
+
+function updateModal(element){
+
+    const modalCard = document.querySelector(".modalCard");
+    const img = modalCard.querySelector("img")
+    const content = modalCard.querySelector(".contentCard");
+    const name = content.querySelector("#nameModal");
+    const email = content.querySelector("#emailModal");
+    const city = content.querySelector("#cityModal");
+    const number = content.querySelector("#numberModal");
+    const address = content.querySelector("#addressModal");
+    const birthday = content.querySelector("#birthdayModal");
+
+    img.src = element.dataset.imgLarge;
+    name.textContent = element.dataset.name;
+    email.textContent = element.dataset.email;
+    city.textContent = element.dataset.city;
+    number.textContent = element.dataset.phone;
+    address.textContent = element.dataset.address;
+    birthday.textContent = element.dataset.birthday;
+
+}
+
+function nextCard(){
+
+    if (index !== employeesFiltered.length - 1){
+
+        index++;
+        employee = employeesFiltered[index];
+        
+
+    }else{
+
+        index = 0;
+        employee = employeesFiltered[index];
+
+    }
+
+    updateModal(employee);
+}
+
+function previusCard(){
+
+    if(index !== 0){
+
+        index--;
+        employee = employeesFiltered[index];
+
+    }else{
+
+        index = employeesFiltered.length - 1;
+        employee = employeesFiltered[index];
+
+    }
+
+    updateModal(employee);
+
+}
+
+window.nextCard = nextCard;
+window.previusCard = previusCard;
 
 employeesContainer.addEventListener("click", event => {
 
@@ -9,7 +72,7 @@ employeesContainer.addEventListener("click", event => {
 
     if(!card) return;
 
-    const index = Array.from(employees).indexOf(card);
+    index = Array.from(employeesFiltered).indexOf(card);
 
     overlay.classList.remove("close");
     overlay.classList.add("open");
@@ -34,22 +97,29 @@ employeesContainer.addEventListener("click", event => {
 
     const name = document.createElement("h3");
     name.textContent = card.dataset.name;
+    name.id = "nameModal";
 
     const email = document.createElement("p");
     email.textContent = card.dataset.email;
+    email.id = "emailModal";
 
     const city = document.createElement("p");
     city.textContent = card.dataset.city;
+    city.id = "cityModal";
 
     const line = document.createElement("hr");
 
     const number = document.createElement("p");
     number.textContent = card.dataset.phone;
+    number.id = "numberModal";
 
     const address = document.createElement("p");
     address.textContent = card.dataset.address;
+    address.id = "addressModal";
 
     const birthday = document.createElement("p");
+    birthday.textContent = "Birthday: " + card.dataset.birthday;
+    birthday.id = "birthdayModal";
 
     [name, email, city, line, number, address, birthday].forEach(element => contentCard.appendChild(element));
     
